16b52a97b36b7d5dd933713dd9a83551dc3ce1a0
ThornDev <jiemianwang@gmail.com>
2016-12-30 21:05:45 +0800

    add mark

--- TBModule/ViewController.m
+++ TBModule/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+#pragma mark - Life Circle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
